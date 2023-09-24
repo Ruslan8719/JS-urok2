ff9806dd20f2b8445ab497f5abb6d13508e43f30
Tyulin Ruslan Vladimiroviсh <Tyulin_r@itstep.edu.az>
2023-09-24 12:45:56 +0400

    3 zadaniye

--- main.js
+++ main.js
@@ -26,3 +26,50 @@
 // else if (result > 0){
 //  console.log ("even");
 // }
+
+// let number1 = 2;
+// let number2 = 2;
+
+// if (number1===number2){
+//     console.log ("ravni");
+// }
+
+// if (number1>number2){
+//     console.log ("number1 bolshe");
+// }
+
+// else if (number1<number2){
+//     console.log ("number2 bolshe");
+// }
+
+
+const mark = 71;
+
+if (mark <= 40 && mark >=0){
+    console.log ("F");
+}
+
+else if (mark <= 50 && mark >40){
+    console.log ("E");
+}
+
+else if (mark <= 60 && mark >50){
+    console.log ("D");
+}
+
+else if (mark <= 70 && mark >60){
+    console.log ("C");
+}
+
+else if (mark <= 80 && mark >70){
+    console.log ("B");
+}
+
+else if (mark <= 90 && mark >80){
+    console.log ("A");
+}
+
+else if (mark <= 100 && mark > 90){
+    console.log ("S");
+}
+
