6f2d35de751fe0628fbc311ae78c97301abcd3ae
Tyulin Ruslan Vladimiroviсh <Tyulin_r@itstep.edu.az>
2023-09-24 12:05:20 +0400

    zadaniya

---
+++ main.js
@@ -0,0 +1,28 @@
+// let budget = 10;
+// let breadPrice = 40;
+// let milkPrice = 30;
+
+// if (budget >= breadPrice) {
+//     console.log ("Я могу купить хлеб");
+//     budget = budget-breadPrice;
+// } 
+
+// else if (budget >= milkPrice) {
+//     console.log ("Я могу купить молоко");
+//     budget = budget - milkPrice;
+// }
+
+// else {
+//     console.log ( "Нищебродка");
+// }
+
+// let number=110;
+// let result = number % 2;
+
+// if (result === 0){
+//     console.log ("odd");
+// }
+
+// else if (result > 0){
+//  console.log ("even");
+// }
